af7c208699bee6afb60c119fbe520a853dc3c185
lcyanxi <845892601@qq.com>
2018-02-24 18:53:41 +0800

    add-jquery-datables

--- springbootTest/src/main/java/douguo/web/webController.java
+++ springbootTest/src/main/java/douguo/web/webController.java
@@ -1,18 +1,77 @@
 package douguo.web;
 
+import douguo.model.Info;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lcyanxi on 2018/2/14.
  */
-@Controller
-public class webController {
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String hello(Model model) {
+@Controller public class webController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET) public String hello(Model model) {
         model.addAttribute("name", "Dear");
         return "home";
     }
+
+    @RequestMapping(value = "/tableDemoAjax",method = RequestMethod.POST)
+    @ResponseBody
+    public String tableDemoAjax(@RequestParam String aoData) {
+        System.out.println("enter tableDemoAjax");
+        System.out.println("aoData--->"+aoData);
+        JSONArray jsonarray = JSONArray.fromObject(aoData);
+        String sEcho = null;      //记录操作的次数
+        int iDisplayStart = 0;    // 起始索引
+        int iDisplayLength = 0;   // 每页显示的行数
+
+
+        //这里获取从前台传递过来的参数，从而确保是否分页、是否进行查询、是否排序等
+        for (int i = 0; i < jsonarray.size(); i++) {
+            JSONObject obj = (JSONObject) jsonarray.get(i);
+            if (obj.get("name").equals("sEcho")){
+                sEcho = obj.get("value").toString();
+            }
+
+            if (obj.get("name").equals("iDisplayStart")){
+                iDisplayStart = obj.getInt("value");
+            }
+
+            if (obj.get("name").equals("iDisplayLength")){
+                iDisplayLength = obj.getInt("value");
+            }
+        }
+
+        // 生成20条测试数据，在实际开发中这里的数据应该是根据分页、排序、查询等情况从数据库中进行查询的
+        List<String[]> lst = new ArrayList<String[]>();
+        List<Info> list=new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Info info=new Info();
+            info.setAddress("address"+i);
+            info.setFirstname("firstname"+i);
+            info.setLastname("lastname"+i);
+            info.setId(i);
+            list.add(info);
+        }
+
+        JSONObject getObj = new JSONObject();
+        System.out.println("sEcho:"+sEcho);
+        System.out.println("iDisplayStart"+iDisplayStart+":iDisplayStart+iDisplayLength"+iDisplayStart+iDisplayLength);
+        getObj.put("sEcho", sEcho);
+        getObj.put("iTotalRecords", list.size());          //实际的行数
+        getObj.put("iTotalDisplayRecords", list.size());   //显示的行数,这个要和上面写的一样
+        getObj.put("aaData", list.subList(iDisplayStart,iDisplayStart+iDisplayLength));//要以JSON格式返回，否则前台没法显示
+        System.out.println(getObj);
+        return getObj.toString();
+    }
+
+
 }
---
+++ springbootTest/target/classes/static/js/dynamic-table.js
@@ -0,0 +1,109 @@
+var Script = function () {
+
+        // begin first table
+        $('#sample_1').dataTable({
+            "sDom": "<'row'<'col-sm-6'l><'col-sm-6'f>r>t<'row'<'col-sm-6'i><'col-sm-6'p>>",
+            "sPaginationType": "bootstrap",
+            "oLanguage": {
+                "sLengthMenu": "每页 _MENU_ 项",
+                "sSearch": "搜索:",
+                "sInfo": "当前显示第 _START_ 至 _END_ 项，共 _TOTAL_ 项。",
+                "sInfoEmpty": "当前显示第 0 至 0 项，共 0 项",
+                "sEmptyTable": "表中数据为空",
+                "sZeroRecords": "没有匹配结果",
+                "sProcessing": "处理中...",
+                "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+                "sInfoPostFix": "",
+                "sLoadingRecords": "载入中...",
+                "oPaginate": {
+                    "sPrevious": "上一页",
+                    "sNext": "下一页"
+
+                }
+            },
+            "iDisplayLength":50,//每页的行数，每页默认数量:10
+            "bAutoWidth": false, //自适应宽度
+            "bFilter": false,//是否显示搜索框
+            "bProcessing": false,                   // 是否显示取数据时的那个等待提示
+            "bServerSide": true,                    //这个用来指明是通过服务端来取数据
+            "sAjaxSource": "/tableDemoAjax",      //这个是请求的地址
+            "fnServerData": retrieveData,            // 获取数据的处理函数
+
+            //配置列要显示的数据
+            "columns": [
+                { "data": null},
+                { "data": "firstname" },
+                { "data": "lastname" },
+                { "data": "address" },
+                { "data": "" }//操作按钮列
+            ],
+
+            //按钮列
+            "columnDefs": [{
+                    targets: 0,
+                    defaultContent: ""
+                },
+                {
+                    "targets": -1,//删除
+                    "data": null,
+                    "defaultContent": "<button id='editrow' class='btn btn-primary' type='button'><i class='fa fa-edit'></i>编辑</button>" +
+                    "<button id='delrow' class='btn btn-primary' type='button'><i class='fa fa-trash-o'></i>删除</button>"
+                }] ,
+            "oAria": {
+                "sSortAscending": ": 以升序排列此列",
+                "sSortDescending": ": 以降序排列此列"
+            }
+
+        });
+
+    // 3个参数的名字可以随便命名,但必须是3个参数,少一个都不行
+    function retrieveData(sSource, aoData, fnCallback) {
+        $.ajax({
+            url: sSource,                              //这个就是请求地址对应sAjaxSource
+            data: {"aoData": JSON.stringify(aoData)},   //这个是把datatable的一些基本数据传给后台,比如起始位置,每页显示的行数 ,分页,排序,查询等的值
+            type: 'post',
+            dataType: 'json',
+            async: false,
+            success: function (result) {
+                fnCallback(result);                     //把返回的数据传给这个方法就可以了,datatable会自动绑定数据的
+            },
+            error: function (msg) {
+            }
+        });
+    }
+
+        jQuery('#sample_1 .group-checkable').change(function () {
+            var set = jQuery(this).attr("data-set");
+            var checked = jQuery(this).is(":checked");
+            jQuery(set).each(function () {
+                if (checked) {
+                    $(this).attr("checked", true);
+                } else {
+                    $(this).attr("checked", false);
+                }
+            });
+            jQuery.uniform.update(set);
+        });
+
+        jQuery('#sample_1_wrapper .dataTables_filter input').addClass("form-control"); // modify table search input
+        jQuery('#sample_1_wrapper .dataTables_length select').addClass("form-control"); // modify table per page dropdown
+
+
+    jQuery(document).ready(function () {
+
+        //按钮的绑定事件要放到document或者其他父标签上，因为元素是在datatable方法加载完成之后才显示出来的
+        //编辑
+        $(".portlet-body").on('click', 'button#editrow', function () {
+
+            var data = $("#sample_1").DataTable().row($(this).parents("tr")).data();
+            alert(data.firstname + "'s Division is: ");
+        });
+        //删除
+        $(".portlet-body").on('click', 'button#delrow', function () {
+
+            var data = $("#sample_1").DataTable().row($(this).parents("tr")).data();
+            alert("Do you want delete " + data.firstname + "?");
+        });
+
+    });
+}();
